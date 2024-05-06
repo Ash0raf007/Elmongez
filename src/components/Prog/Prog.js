011e5bfa9d9a4d82f1898195ca3f35f35e79b9b1
Ash0raf007 <Ashrafkadry07@gmail.com>
2024-05-06 19:44:05 +0200

    v3

--- src/components/Prog/Prog.js
+++ src/components/Prog/Prog.js
@@ -1,39 +1,42 @@
-import React from 'react'
+import React from "react";
 import data from "./data.json";
 import icon from "../../assets/icon.png";
-import './prog.css'
+import "./prog.css";
 function Prog() {
   return (
     <div className="contai">
-        <div className="container">
-              <div className="intro">
-    <h1 style={{color:"white"}} className="text"> قسم البرمجه</h1>
-    </div>
-    {data.map((item) => (
-      <div key={item.id} className="info">
-        <details className="deti">
-        <summary>
-          <h4 className="name">{item.name}</h4>
-        </summary>
-        <p className="title">&#9737;{item.title1}</p>
-        <p className="title">&#9737;{item.title2} </p>
+      <div className="container">
+        <div className="intro">
+          <h1 style={{ color: "white" }} className="text">
+            
+            قسم البرمجه
+          </h1>
+        </div>
+        {data.map((item) => (
+          <div key={item.id} className="info">
+            <details className="deti">
+              <summary>
+                <h4 className="name">{item.name}</h4>
+              </summary>
+              <p className="title">&#9737;{item.title1}</p>
+              <p className="title">&#9737;{item.title2} </p>
 
-        <a
-          href="https://api.whatsapp.com/send?phone=201009625904"
-          alt=""
-          target="_blank"
-          rel="noreferrer"
-className='contact'
-        >
-          <span>تواصل معنا</span>
-          <img src={icon} width={30} alt="" />
-        </a>
-      </details>
+              <a
+                href="https://api.whatsapp.com/send?phone=201009625904"
+                alt=""
+                target="_blank"
+                rel="noreferrer"
+                className="contact"
+              >
+                <span>تواصل معنا</span>
+                <img src={icon} width={30} alt="" />
+              </a>
+            </details>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-  </div>
-  )
+    </div>
+  );
 }
 
-export default Prog
+export default Prog;
